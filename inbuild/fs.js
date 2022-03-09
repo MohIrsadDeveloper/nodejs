1eee5674dd956da52cecb989d85cc52721d5e252
Idrisi Moh Irsad <idrisimohdirshad@gmail.com>
2022-03-09 08:33:42 +0530

    express

--- inbuild/fs.js
+++ inbuild/fs.js
@@ -1,11 +1,36 @@
 var fs = require('fs');
 
 // create new file
-fs.writeFile('mycode.txt', "This is node class", function (err) {
+/* fs.writeFile('mycode.txt', "This is node class", function (err) {
     if (err) {
         throw err;
     }
     else {
         console.log('File Created');
     }
+}) */
+
+// Create new file & append the text
+/* fs.appendFile('mytext.txt', "My Test Class \n", function (err) {
+    if (err) throw err;
+    console.log("Task Completed");
+})
+ */
+
+// Read File
+/* fs.readFile('mycode.txt', 'utf-8', function (err,data) {
+    if (err) throw err;
+    console.log(data);
+}) */
+
+// Delete File
+/* fs.unlink('mycode.txt', function (err) {
+    if (err) throw err;
+    console.log("File Deleted");
+}) */
+
+// Rename File
+fs.rename('mytext.txt', 'mycode.txt', function (err) {
+    if (err) throw err;
+    console.log("File Renamed");
 })
---
+++ inbuild/http.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+// req >>> what we will send to server
+// res >>> what we receive in return 
+
+var server = http.createServer(function (req, res) {
+    res.write('<h1>This is node server2</h1>')
+})
+
+server.listen(4000)
